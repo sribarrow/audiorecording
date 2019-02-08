cb3e3c046d84ef2841aa98cca2d343795f69b99d
sripriya <sribarrow@gmail.com>
2019-02-08 04:53:30 +0000

    timer

--- assets/recording.js
+++ assets/recording.js
@@ -12,8 +12,17 @@ stop.disabled = true;
 
 // visualiser setup - create web audio api context and canvas
 
-var audioCtx = new(window.AudioContext || webkitAudioContext)();
+var AudioContext = window.AudioContext || window.webkitAudioContext;
+//var audioCtx = new AudioContext();
+var audioCtx, analyser;
+var dataArray;
+var time = document.querySelector('#time');
+//console.log(time.innerHTML);
+var timer, seconds;
+//seconds = 40;
+
 var canvasCtx = canvas.getContext("2d");
+//var analyser = audioCtx.createAnalyser();
 // check if getUserMedia is available in the browser
 if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
     console.log('getUserMedia supported.');
@@ -31,30 +40,44 @@ if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
 
         record.onclick = function() {
             mediaRecorder.start();
+            seconds = 40;
             console.log(mediaRecorder.state);
             console.log("recorder started");
-            record.style.background = "red";
-
+            //record.style.background = "green";
+            // stop.style.background = "red";
+            record.disabled = true;
             stop.disabled = false;
             record.disabled = true;
+            timer = setInterval(myTimer, 1000);
         }
+
+        function myTimer() {
+            seconds--;
+            time.innerHTML = seconds;
+            if (seconds === 0) {
+                stop.onclick();
+            }
+        }
+
 
         stop.onclick = function() {
             mediaRecorder.stop();
+            seconds = 0;
+            clearInterval(timer);
             console.log(mediaRecorder.state);
             console.log("recorder stopped");
-            record.style.background = "";
-            record.style.color = "";
+            //record.style.background = "";
+            // stop.style.background = "";
             // mediaRecorder.requestData();
-
             stop.disabled = true;
             record.disabled = false;
+
         }
 
         mediaRecorder.onstop = function(e) {
                 console.log("data available after MediaRecorder.stop() called.");
                 var clipName = prompt('Enter a name for your sound clip?', 'Unnamed audio');
-                console.log(clipName);
+                //console.log(clipName);
                 var clipContainer = document.createElement('article');
                 var clipLabel = document.createElement('p');
                 var audio = document.createElement('audio');
@@ -115,58 +138,46 @@ if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
 }
 
 function visualize(stream) {
+    audioCtx = new AudioContext();
     var source = audioCtx.createMediaStreamSource(stream);
-
-    var analyser = audioCtx.createAnalyser();
-    analyser.fftSize = 2048;
-    var bufferLength = analyser.frequencyBinCount;
-    var dataArray = new Uint8Array(bufferLength);
-
+    analyser = audioCtx.createAnalyser();
+    analyser.minDecibels = -70;
+    analyser.maxDecibels = -10;
+    analyser.smoothingTimeConstant = 0.95;
     source.connect(analyser);
-    //analyser.connect(audioCtx.destination);
+    analyser.connect(audioCtx.destination);
+    draw();
+}
 
-    draw()
 
-    function draw() {
-        WIDTH = canvas.width
-        HEIGHT = canvas.height;
-        //browser method to perform animation / next repaint
-        requestAnimationFrame(draw);
+function draw() {
+    WIDTH = canvas.width
+    HEIGHT = canvas.height;
+    //browser method to perform animation / next repaint
+    requestAnimationFrame(draw);
+    analyser.fftSize = 256;
+    var bufferLength = analyser.frequencyBinCount;
+    //console.log(bufferLength);
+    dataArray = new Uint8Array(bufferLength);
+    analyser.getByteFrequencyData(dataArray);
+    canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
+    canvasCtx.fillStyle = 'rgb(200, 200, 200)';
+    var barWidth = (WIDTH / bufferLength);
+    var barHeight;
+    var x = 0;
+    for (var i = 0; i < bufferLength; i++) {
+        barHeight = dataArray[i] * .85;
 
-        analyser.getByteTimeDomainData(dataArray);
+        canvasCtx.fillStyle = 'rgb(' + (barHeight + 100) + ',50,50)';
+        canvasCtx.fillRect(x, HEIGHT - barHeight / 2, barWidth, barHeight / 2);
 
-        canvasCtx.fillStyle = 'rgb(200, 200, 200)';
-        canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
-
-        canvasCtx.lineWidth = 2;
-        canvasCtx.strokeStyle = 'rgb(0, 0, 0)';
-
-        canvasCtx.beginPath();
-
-        var sliceWidth = WIDTH * 1.0 / bufferLength;
-        var x = 0;
-
-        for (var i = 0; i < bufferLength; i++) {
-
-            var v = dataArray[i] / 128.0;
-            var y = v * HEIGHT / 2;
-
-            if (i === 0) {
-                canvasCtx.moveTo(x, y);
-            } else {
-                canvasCtx.lineTo(x, y);
-            }
-
-            x += sliceWidth;
-        }
-        canvasCtx.lineTo(canvas.width, canvas.height / 2);
-        canvasCtx.stroke();
-
+        x += barWidth + 1;
     }
+
 }
 
 window.onresize = function() {
     canvas.width = mainSection.offsetWidth;
 }
 
-window.onresize();
+window.onresize()
